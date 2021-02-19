33f36b57fad7e4bfbb069c77943abf26d6f7669e
kollinbadams <40585343+kollinbadams@users.noreply.github.com>
2021-02-19 16:12:09 -0500

    initial commit

---
+++ popup.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var checkForUserMediaButton = document.getElementById('checkUserMediaButton');
+    checkForUserMediaButton.addEventListener('click',
+        function () {
+        if (hasGetUserMedia()) {
+            alert("getUserMedia() is supported by your browser");
+            getMediaSourcesAvailable();
+            
+        } else {
+            alert("getUserMedia() is not supported by your browser");
+        } });
+}, false);
+
+
+function hasGetUserMedia() {
+    return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+}
+
+
+
+function getMediaSourcesAvailable() {
+    var devices = navigator.mediaDevices.enumerateDevices();
+    devices.then(function (devices) {
+        for (var i = 0; i < devices.length; i++) {
+            var device = devices[i];
+            if (device.kind === 'videoinput') {
+                var deviceID = device.deviceId.toString();
+                var label = device.label;
+                //var deviceData = "<tr><td>" + deviceID + "</td><td>" + label + "</td></tr>";
+
+                var tbodyRef = document.getElementById('mediaSourceTable').getElementsByTagName('tbody')[0];
+
+                // Insert a row at the end of table
+                var newRow = tbodyRef.insertRow();
+
+                // Insert a cell at the end of the row
+                var deviceIdCell = newRow.insertCell();
+                // Append a text node to the cell
+                var deviceIdText = document.createTextNode(deviceID);
+
+                deviceIdCell.appendChild(deviceIdText);
+
+                var deviceLabelCell = newRow.insertCell();
+                var deviceLabelText = document.createTextNode(label);
+                deviceLabelCell.appendChild(deviceLabelText);
+            }
+        };
+    });
+}
